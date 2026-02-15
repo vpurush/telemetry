025bd0ff0dd5f76113e0cec807f2ea2442183562
Vijay Purush <vijayaraghavan1987@gmail.com>
2026-02-15 23:38:00 +1100

    added few more fields to the logs

--- cdk/lib/telemetry-stack.ts
+++ cdk/lib/telemetry-stack.ts
@@ -5,7 +5,7 @@ import * as ApiGateway from "aws-cdk-lib/aws-apigateway";
 import * as Logs from "aws-cdk-lib/aws-logs";
 import * as SQS from "aws-cdk-lib/aws-sqs";
 import * as IAM from "aws-cdk-lib/aws-iam";
-import { RustFunction } from 'cargo-lambda-cdk';
+import { RustFunction } from "cargo-lambda-cdk";
 import { Construct } from "constructs";
 import * as LambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
 
@@ -26,10 +26,14 @@ export class TelemetryStack extends cdk.Stack {
       queueName: "telemetry-data-queue",
     });
 
-    const telemetryApiLogGroup = new Logs.LogGroup(this, "TelemetryApiLogGroup", {
-      logGroupName: "/aws/apigateway/telemetry-api",
-      retention: Logs.RetentionDays.ONE_MONTH,
-    });
+    const telemetryApiLogGroup = new Logs.LogGroup(
+      this,
+      "TelemetryApiLogGroup",
+      {
+        logGroupName: "/aws/apigateway/telemetry-api",
+        retention: Logs.RetentionDays.ONE_MONTH,
+      },
+    );
 
     const telemetryApiGateway = new ApiGateway.RestApi(
       this,
@@ -37,14 +41,34 @@ export class TelemetryStack extends cdk.Stack {
       {
         restApiName: "telemetry-api",
         deployOptions: {
-          accessLogDestination: new ApiGateway.LogGroupLogDestination(telemetryApiLogGroup),
-          accessLogFormat: ApiGateway.AccessLogFormat.custom("{\"requestId\":\"$context.requestId\",\"ip\":\"$context.identity.sourceIp\",\"user\":\"$context.identity.user\",\"caller\":\"$context.identity.caller\",\"requestTime\":\"$context.requestTime\",\"httpMethod\":\"$context.httpMethod\",\"resourcePath\":\"$context.resourcePath\",\"status\":\"$context.status\",\"protocol\":\"$context.protocol\",\"responseLength\":\"$context.responseLength\", \"errorMessage\": \"$context.error.message\", \"errorMessageString\": \"$context.error.messageString\", \"errorType\": \"$context.error.type\", \"errorCause\": \"$context.error.cause\" }"),
-          loggingLevel: ApiGateway.MethodLoggingLevel.INFO,          
+          accessLogDestination: new ApiGateway.LogGroupLogDestination(
+            telemetryApiLogGroup,
+          ),
+          accessLogFormat: ApiGateway.AccessLogFormat.custom(
+            JSON.stringify({
+              requestId: "$context.requestId",
+              ip: "$context.identity.sourceIp",
+              user: "$context.identity.user",
+              caller: "$context.identity.caller",
+              requestTime: "$context.requestTime",
+              httpMethod: "$context.httpMethod",
+              resourcePath: "$context.resourcePath",
+              status: "$context.status",
+              protocol: "$context.protocol",
+              responseLength: "$context.responseLength",
+              errorMessage: "$context.error.message",
+              errorMessageString: "$context.error.messageString",
+              errorType: "$context.error.type",
+              errorCause: "$context.error.cause",
+              integrationError: "$context.integration.error",
+              integrationStatus: "$context.integration.status",
+            }),
+          ),
+          loggingLevel: ApiGateway.MethodLoggingLevel.INFO,
           metricsEnabled: true,
         },
       },
     );
-    
 
     const telemetryApiGatewayRole = new IAM.Role(
       this,
@@ -65,7 +89,7 @@ export class TelemetryStack extends cdk.Stack {
         },
         requestTemplates: {
           // VTL template to transform the JSON body into a URL-encoded format
-          "application/json": `Action=SendMessage&MessageBody=$input.body`,
+          "application/json": `Action=SendMessage&MessageBody=$util.urlEncode($input.body)`,
         },
         integrationResponses: [
           {
@@ -83,18 +107,27 @@ export class TelemetryStack extends cdk.Stack {
       value: telemetryApiGateway.url,
     });
 
-    const telemetrySQSToS3RustFunction = new RustFunction(this, 'TelemetrySQSToS3RustFunction', {
-      manifestPath: '../telemetry-sqs-to-s3/Cargo.toml',
-    });
+    const telemetrySQSToS3RustFunction = new RustFunction(
+      this,
+      "TelemetrySQSToS3RustFunction",
+      {
+        manifestPath: "../telemetry-sqs-to-s3/Cargo.toml",
+      },
+    );
 
-    telemetrySQSToS3RustFunction.addEnvironment('TELEMETRY_TEMPORARY_BUCKET_NAME', telemetryTemporaryBucket.bucketName);
+    telemetrySQSToS3RustFunction.addEnvironment(
+      "TELEMETRY_TEMPORARY_BUCKET_NAME",
+      telemetryTemporaryBucket.bucketName,
+    );
 
     telemetryTemporaryBucket.grantReadWrite(telemetrySQSToS3RustFunction);
 
     // Add sqs trigger to the function
-    telemetrySQSToS3RustFunction.addEventSource(new LambdaEventSources.SqsEventSource(telemetryQueue, {
-      batchSize: 1000,
-      maxBatchingWindow: cdk.Duration.seconds(1), // Change it to 1hr later
-    }));
+    telemetrySQSToS3RustFunction.addEventSource(
+      new LambdaEventSources.SqsEventSource(telemetryQueue, {
+        batchSize: 1000,
+        maxBatchingWindow: cdk.Duration.seconds(1), // Change it to 1hr later
+      }),
+    );
   }
 }
